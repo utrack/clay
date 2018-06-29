27a634f6d0ec8e158a0432d978ef77de8ce084dc
Koptelov Nikita <nkoptelov@ozon.ru>
2018-06-29 14:10:53 +0300

    added HTTP resp headers integration test

--- integration/go.mod
+++ integration/go.mod
@@ -1,8 +1,10 @@
 module github.com/utrack/clay/integration
 
-replace github.com/utrack/clay/v2 v2.0.0 => ../
+replace github.com/utrack/clay v1.0.2 => github.com/utrack/clay/v2 v2.1.0
+
+replace github.com/utrack/clay/v2 v2.1.0 => ../
 
 require (
 	github.com/jmoiron/jsonq v0.0.0-20150511023944-e874b168d07e
-	github.com/utrack/clay/v2 v2.0.0
+	github.com/utrack/clay/v2 v2.1.0
 )
---
+++ integration/http_headers_response/pb/strings/strings_test.go
@@ -0,0 +1,135 @@
+package strings
+
+import (
+	"bytes"
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/utrack/clay/v2/transport"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
+)
+
+// assert type for the headers
+type a struct {
+	Name   string
+	Values []string
+}
+
+// TestHTTPHeadersPass_vanillaHTTP tests that default HTTP middleware passes HTTP headers to
+// gRPC metadata.
+func TestHTTPHeadersPass_vanillaHTTP(t *testing.T) {
+	so := assert.New(t)
+	impl, ts := getTestSvc()
+	defer ts.Close()
+
+	tc := []a{
+		a{
+			Name:   "X-Something-For-Test",
+			Values: []string{"val1", "Val2"},
+		},
+		a{
+			Name:   "User-Agent",
+			Values: []string{"Go-http-client/1.1"},
+		},
+	}
+
+	calledFunc := false
+
+	impl.f = func(ctx context.Context, req *String) (*String, error) {
+		calledFunc = true
+
+		for _, c := range tc {
+			md := metadata.New(map[string]string{})
+
+			for i := range c.Values {
+				md.Append(c.Name, c.Values[i])
+			}
+			grpc.SetHeader(ctx, md)
+		}
+
+		return nil, nil
+	}
+
+	req, err := http.NewRequest("POST", ts.URL+pattern_goclay_Strings_ToLower_0_builder(), bytes.NewReader([]byte("{}")))
+	so.Nil(err)
+
+	rsp, err := http.DefaultClient.Do(req)
+	so.Nil(err)
+	so.True(calledFunc)
+
+	for _, c := range tc {
+		so.EqualValues(c.Values, rsp.Header[c.Name])
+	}
+}
+
+// TestHTTPHeadersPass_genClient tests that default HTTP middleware passes HTTP headers to
+// gRPC metadata using generated client.
+func TestHTTPHeadersPass_genClient(t *testing.T) {
+	so := assert.New(t)
+	impl, ts := getTestSvc()
+	defer ts.Close()
+
+	tc := []a{
+		a{
+			Name:   "User-Agent",
+			Values: []string{"Go-http-client/1.1"},
+		},
+		a{
+			Name:   "Accept",
+			Values: []string{"application/json"},
+		},
+	}
+
+	calledFunc := false
+
+	impl.f = func(ctx context.Context, req *String) (*String, error) {
+		calledFunc = true
+		md, ok := metadata.FromIncomingContext(ctx)
+		so.True(ok)
+
+		for _, c := range tc {
+			got := md.Get(strings.ToLower(c.Name))
+			so.EqualValues(c.Values, got)
+		}
+
+		return &String{}, nil
+	}
+
+	cli := NewStringsHTTPClient(http.DefaultClient, ts.URL)
+	_, err := cli.ToLower(context.Background(), &String{})
+	so.Nil(err)
+	so.True(calledFunc)
+}
+
+func getTestSvc() (*StringsImplementation, *httptest.Server) {
+	mux := http.NewServeMux()
+	impl := NewStrings()
+	d := impl.GetDescription()
+	d.RegisterHTTP(mux)
+
+	ts := httptest.NewServer(mux)
+	return impl, ts
+}
+
+type StringsImplementation struct {
+	f func(ctx context.Context, req *String) (*String, error)
+}
+
+func NewStrings() *StringsImplementation {
+	return &StringsImplementation{}
+}
+
+func (i *StringsImplementation) ToLower(ctx context.Context, req *String) (*String, error) {
+	return i.f(ctx, req)
+}
+
+// GetDescription is a simple alias to the ServiceDesc constructor.
+// It makes it possible to register the service implementation @ the server.
+func (i *StringsImplementation) GetDescription() transport.ServiceDesc {
+	return NewStringsServiceDesc(i)
+}
