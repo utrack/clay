cc2e1896920225240d56362455e4dc815c830bb2
Ivan Korolev <i.d.korolev@gmail.com>
2018-07-20 17:54:06 +0300

    Fix generated files header - fixes #37

--- integration/http_headers_response/main.go
+++ integration/http_headers_response/main.go
@@ -4,7 +4,7 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi"
-	"github.com/utrack/clay/v2/integration/http_headers_response/strings"
+	"github.com/utrack/clay/integration/http_headers_response/strings"
 )
 
 func main() {
