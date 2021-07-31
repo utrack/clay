7cf8880f31a50a60a4451a20ceeb5e158222cca6
bullgare <bullgare@gmail.com>
2021-07-31 15:59:07 +0300

    Migrates to google.golang.org/protobuf aka protobuf-v2. phase 5. updated example and added a readme to it

--- doc/example/go.mod
+++ doc/example/go.mod
@@ -7,13 +7,13 @@ replace github.com/utrack/clay/v2 => ../../
 require (
 	github.com/go-chi/chi v3.3.4+incompatible
 	github.com/go-openapi/spec v0.0.0-20180415031709-bcff419492ee
-	github.com/golang/protobuf v1.3.2
-	github.com/grpc-ecosystem/grpc-gateway v1.14.2
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.5.0
 	github.com/pkg/errors v0.8.1
 	github.com/rakyll/statik v0.1.1
 	github.com/sirupsen/logrus v1.4.2
 	github.com/utrack/clay/v2 v2.4.7
-	golang.org/x/net v0.0.0-20191002035440-2ec189313ef0
-	google.golang.org/genproto v0.0.0-20190927181202-20e1ac93f88c
-	google.golang.org/grpc v1.27.1
+	google.golang.org/genproto v0.0.0-20210617175327-b9e0b3197ced
+	google.golang.org/grpc v1.38.0
+	google.golang.org/protobuf v1.27.1
+	gopkg.in/yaml.v2 v2.2.3 // indirect
 )
--- doc/example/main.go
+++ doc/example/main.go
@@ -1,17 +1,14 @@
 package main
 
 import (
-	"context"
-	"errors"
 	"net/http"
 
 	"github.com/go-chi/chi"
 	"github.com/rakyll/statik/fs"
 	"github.com/sirupsen/logrus"
+	sum "github.com/utrack/clay/doc/example/implementation"
 
-	pb "github.com/utrack/clay/doc/example/pb"
 	"github.com/utrack/clay/v2/log"
-	"github.com/utrack/clay/v2/transport"
 	"github.com/utrack/clay/v2/transport/middlewares/mwgrpc"
 	"github.com/utrack/clay/v2/transport/server"
 
@@ -19,31 +16,6 @@ import (
 	// for the sake of example.
 	_ "github.com/utrack/clay/doc/example/static/statik"
 )
-
-// SumImpl is an implementation of SummatorService.
-type SumImpl struct{}
-
-// Sum implements SummatorServer.Sum.
-func (s *SumImpl) Sum(ctx context.Context, r *pb.SumRequest) (*pb.SumResponse, error) {
-	if r.GetA() == 0 {
-		return nil, errors.New("a is zero")
-	}
-
-	if r.GetB().B == 65536 {
-		panic(errors.New("we've got a problem"))
-	}
-
-	sum := r.GetA() + r.GetB().B
-	return &pb.SumResponse{
-		Sum: sum,
-	}, nil
-}
-
-// GetDescription is a simple alias to the ServiceDesc constructor.
-// It makes it possible to register the service implementation @ the server.
-func (s *SumImpl) GetDescription() transport.ServiceDesc {
-	return pb.NewSummatorServiceDesc(s)
-}
 
 func main() {
 	// Wire up our bundled Swagger UI
@@ -54,7 +26,7 @@ func main() {
 	hmux := chi.NewRouter()
 	hmux.Mount("/", http.FileServer(staticFS))
 
-	impl := &SumImpl{}
+	impl := sum.NewSummator()
 	srv := server.NewServer(
 		12345,
 		// Pass our mux with Swagger UI
