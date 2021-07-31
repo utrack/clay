8654be68fb3f7ebbd3d9398219e72bca677b9c66
bullgare <bullgare@gmail.com>
2021-07-31 15:40:00 +0300

    Migrates to google.golang.org/protobuf aka protobuf-v2. phase 4. cleaned integration makefiles

--- integration/env.mk
+++ integration/env.mk
@@ -11,31 +11,9 @@ GEN_GO_BIN:=$(DIR)/bin/protoc-gen-go
 export GEN_GO_BIN
 GEN_GO_GRPC_BIN:=$(DIR)/bin/protoc-gen-go-grpc
 export GEN_GO_GRPC_BIN
-GEN_GOFAST_BIN:=$(DIR)/bin/protoc-gen-gofast
-export GEN_GOFAST_BIN
-GEN_GOGOFAST_BIN:=$(DIR)/bin/protoc-gen-gogofast
-export GEN_GOGOFAST_BIN
 
-#GRPC_GATEWAY_PKG:=$(shell go list -m all | grep github.com/grpc-ecosystem/grpc-gateway/v2 | awk '{print ($$4 != "" ? $$4 : $$1)}')
-#GRPC_GATEWAY_VERSION:=$(shell go list -m all | grep github.com/grpc-ecosystem/grpc-gateway/v2 | awk '{print ($$5 != "" ? $$5 : $$2)}')
-#GRPC_GATEWAY_PATH:=${FIRST_GOPATH}/pkg/mod/${GRPC_GATEWAY_PKG}@${GRPC_GATEWAY_VERSION}
-#export GRPC_GATEWAY_PATH
-#$(info ${GRPC_GATEWAY_PATH})
 export THIRD_PARTY_PROTO_PATH:=$(dir $(abspath $(lastword $(MAKEFILE_LIST))))third_party/proto
 $(info ${THIRD_PARTY_PROTO_PATH})
-
-#GRPC_GOGO_PROTO_PKG:=$(shell go list -m all | grep github.com/gogo/protobuf | awk '{print ($$4 != "" ? $$4 : $$1)}')
-#GRPC_GOGO_PROTO_VERSION:=$(shell go list -m all | grep github.com/gogo/protobuf | awk '{print ($$5 != "" ? $$5 : $$2)}')
-#GPRC_GOGO_PROTO_PATH:=${FIRST_GOPATH}/pkg/mod/${GRPC_GOGO_PROTO_PKG}@${GRPC_GOGO_PROTO_VERSION}/gogoproto
-#export GPRC_GOGO_PROTO_PATH
-
-#ifeq ($(RELATIVE_PATH_TO_PROTO),)
-#RELATIVE_PATH_TO_PROTO := pb/strings.proto
-#endif
-#
-#ifeq ($(RELATIVE_PATH_IMPL),)
-#RELATIVE_PATH_IMPL := ../strings
-#endif
 
 GREEN:=\033[0;32m
 RED:=\033[0;31m
